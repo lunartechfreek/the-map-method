2081237d458d58acf895a48f3929361a1f1429a5
Daniel Turner <lunartechfreek@googlemail.com>
2024-04-16 19:01:55 +0000

    Start lesson

--- map.js
+++ map.js
@@ -5,10 +5,21 @@
  */
 
 // Using a for loop
+let nums = [ 1, 2, 3, 4, 5 ];
+let results = [];
+for (let num of nums) {
+  results.push(num * 2);
+};
 
+// console.log(results);
 
-// Using map()
+// Using map() ** with visible callback function
+const multByTwo = function (num) {
+  return num * 2;
+}
 
+const mapResults = nums.map(multByTwo);
+console.log(mapResults);
 
 // Simplified w/ map()
 
